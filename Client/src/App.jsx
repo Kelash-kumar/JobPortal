f7dda57e6db83651501fc6383ea0f1b1a614be90
kelash <kelash.raisal@gmail.com>
2024-08-10 18:27:13 +0500

    added the herosection

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,19 +3,26 @@ import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import HeroSection from "./components/home/heroSection";
 function App() {
   return (
     <>
       <Router>
         <Navbar />
+        <HeroSection />
         <Routes>
-          <Route path="/" element={<>home is for details</>} />
+          <Route
+            path="/home"
+            element={
+              <>
+                
+              </>
+            }
+          />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
-     
     </>
   );
 }
---
+++ Client/src/components/home/heroSection.jsx
@@ -0,0 +1,20 @@
+import './HeroSection.css'; // Import the CSS file
+import { CiSearch } from "react-icons/ci";
+const HeroSection = () => {
+    return ( 
+        <div className="hero-container">
+            <div className="hero-content">
+                <span className='headline'>No.1 Job Hunt</span>
+                <h1>Search apply &</h1>
+                <h1>Get Your <span>Dream Job</span></h1>
+                <p>Explore thousands of job opportunities at top companies</p>
+                <div className='search for here'>
+                    <input type="text" name="search" id="search" placeholder='get your job'/>
+                   <span className='search_icon'> <CiSearch/></span>
+                    </div>  
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
